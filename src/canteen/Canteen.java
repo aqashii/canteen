0cec53196009db99b3e9be5da18a26d93e08cd20
drexploit01 <ashiqmuhammedep@gmail.com>
2022-07-03 14:36:33 +0000

    first commit

---
+++ src/canteen/Canteen.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package canteen;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class Canteen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        cnt2 ad= new cnt2();
+        ad.setVisible(true); 
+        
+        // TODO code application logic here
+    }
+
+    private void hide() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
